e8bbc5b7d58f3a1c5614fcabf7e132742e41958d
fexfox <fexfox@vip.qq.com>
2017-07-14 18:29:12 +0800

    更智能的打分方式
    精简所以按钮，仅保留1~5星
    历史 名称 唯一性会自动随机3~5星
    位置与可到达性会自动随机4~5星
    有效避免被频繁人机检测

--- 5 Star One Key.user.js
+++ 5 Star One Key.user.js
@@ -10,9 +10,11 @@
 // ==/UserScript==
 
 var buttons = [
-	{button:"Five Star", total:5, name:5, history:5, unique:5, location:5, safety:5},
-	{button:"553355", total:5, name:5, history:3, unique:3, location:5, safety:5},
-	{button:"533355", total:5, name:3, history:3, unique:3, location:5, safety:5},
+	{button:"5★", total:5},
+    {button:"4★", total:4},
+    {button:"3★", total:3},
+    {button:"2★", total:3},
+    {button:"1★", total:3}
 ];
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -21,23 +23,37 @@ var buttons = [
 
 
 function rate_portal(total, name, history, unique, location, safety) {
+    if(total>1){
     document.querySelector("#AnswersController > form > div:nth-child(1) > div:nth-child(1) > div.btn-group > button:nth-child(" + total + ")").click();
     document.querySelector("#AnswersController > form > div:nth-child(1) > div.col-xs-12.col-sm-4.pull-right.text-center > div:nth-child(5) > button:nth-child(" + name + ")").click();
     document.querySelector("#AnswersController > form > div:nth-child(1) > div.col-xs-12.col-sm-4.pull-right.text-center > div:nth-child(10) > button:nth-child(" + history + ")").click();
     document.querySelector("#AnswersController > form > div:nth-child(1) > div.col-xs-12.col-sm-4.pull-right.text-center > div:nth-child(15) > button:nth-child(" + unique + ")").click();
     document.querySelector("#AnswersController > form > div:nth-child(2) > div:nth-child(1) > div:nth-child(6) > button:nth-child(" + location + ")").click();
     document.querySelector("#AnswersController > form > div:nth-child(2) > div:nth-child(1) > div:nth-child(11) > button:nth-child(" + safety + ")").click();
+    document.querySelector("#submitDiv > button:nth-child(5)").click();
+    }
 }
 
+function rd(n,m){
+    var c = m-n+1;
+    return Math.floor(Math.random() * c + n);
+}
 function add_button() {
-    var button_region = document.getElementById("submitDiv");
+    var list= document.getElementsByClassName("btn-group");
+    for (var i=0; i<list.length; i++){
+    list[i].style.display="none";
+    }
+    var button_region =document.getElementsByClassName("col-xs-12 col-sm-4 text-center")[0];
+    var refnode =document.querySelector("#AnswersController > form > div:nth-child(1) > div:nth-child(1) > br:nth-child(3)");
+    var br = document.createElement("br");
+    button_region.insertBefore(br,refnode);
     buttons.forEach(function(button_data) {
         var button = document.createElement("button");
         var textnode = document.createTextNode(button_data["button"]);
-        button.className = "button big-submit-button";
+        button.className = "button";
         button.appendChild(textnode);
-        button_region.appendChild(button);
-        button.onclick = function(){rate_portal(button_data["total"], button_data["name"], button_data["history"], button_data["unique"], button_data["location"], button_data["safety"]);};
+        button_region.insertBefore(button,refnode);
+        button.onclick = function(){rate_portal(button_data["total"],  rd(3,5), rd(3,5), rd(3,5), rd(4,5), rd(4,5));};
     });
 }
 

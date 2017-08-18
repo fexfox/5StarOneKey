f0d31e6bce6307b25d0a780bcd29b336f659e738
fexfox <fexfox@vip.qq.com>
2017-08-18 11:56:53 +0800

    Update SimpleOPR.user.js

--- SimpleOPR.user.js
+++ SimpleOPR.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         SimpleOPR
-// @version      1.20
+// @version      2.0
 // @description  Simple your OPR
 // @updateURL    https://github.com/fexfox/5StarOneKey/raw/master/SimpleOPR.user.js
 // @downloadURL  https://github.com/fexfox/5StarOneKey/raw/master/SimpleOPR.user.js
